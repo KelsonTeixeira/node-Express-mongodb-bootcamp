2721bfb1feb8b2d5dc338c624a8905dd04fa8a70
kelsonteixeira <txkelson@gmail.com>
2023-07-21 13:02:53 -0300

    commit de teste

---
+++ multiple-awaits.js
@@ -0,0 +1,21 @@
+const superagent = require(`superagent`);
+
+const getValues = () => {
+  const request1 = superagent.get('https://dog.ceo/api/breeds/image/random');
+  const requesta = superagent.get('https://dog.ceo/api/breeds/image/random');
+  const request2 = superagent.get('https://dog.ceo/api/breeds/image/random');
+  const request3 = superagent.get('https://dog.ceo/api/breeds/image/random');
+  return Promise.all([request1, request2, request3]);
+}
+
+const result = async () => {
+  try {
+    const x = await getValues();
+    const imgs = x.map(item => item.body.message);
+    console.log(imgs);
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+result();
